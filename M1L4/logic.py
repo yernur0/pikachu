c2466a302028126c9960970d89e504778677dc63
yernur <123850340+yernur0@users.noreply.github.com>
2024-02-04 15:08:01 +0500

    Add files via upload

---
+++ M1L4/logic.py
@@ -0,0 +1,97 @@
+from random import randint
+from datetime import datetime, timedelta
+import requests
+
+class Pokemon:
+    pokemons = {}
+    # Инициализация объекта (конструктор)
+    def __init__(self, pokemon_trainer):
+
+        self.pokemon_trainer = pokemon_trainer   
+
+        self.pokemon_number = randint(1,1000)
+        self.img = self.get_img()
+        self.name = self.get_name()
+        self.hp = randint(50, 100)
+        self.power = randint(5, 25)
+
+        self.last_feed_time = datetime.now()
+
+        Pokemon.pokemons[pokemon_trainer] = self
+
+    # Метод для получения картинки покемона через API
+    def get_img(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['sprites']['other']['official-artwork']['front_default'])
+        else:
+            return "Pikachu"
+    
+    # Метод для получения имени покемона через API
+    def get_name(self):
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            return (data['forms'][0]['name'])
+        else:
+            return "Pikachu"
+
+
+    # Метод класса для получения информации
+    def info(self):
+        return f"""Имя твоего покеомона: {self.name}
+Сила покемона: {self.hp}
+Здоровье покемона {self.power}"""
+    
+    def attack(self, enemy):
+        if isinstance(enemy, Wizard): # Проверка на то, что enemy является типом данных Wizard (является экземпляром класса Волшебник)
+            chance = randint(1,3)
+            if chance == 1:
+                return "Покемон-волшебник применил щит в сражении"
+        if enemy.hp > self.power:
+            enemy.hp -= self.power
+            return f"Сражение @{self.pokemon_trainer} с @{enemy.pokemon_trainer}"
+        else:
+            enemy.hp = 0
+            return f"Победа @{self.pokemon_trainer} над @{enemy.pokemon_trainer}! "
+    
+    def feed(self, feed_interval = 20, hp_increase = 10 ):
+        current_time = datetime.now()  
+        delta_time = timedelta(seconds=feed_interval)  
+        if (current_time - self.last_feed_time) > delta_time:
+            self.hp += hp_increase
+            self.last_feed_time = current_time
+            return f"Здоровье покемона увеличено. Текущее здоровье: {self.hp}"
+        else:
+            return f"Следующее время кормления покемона: {current_time+delta_time}"  
+
+
+
+class Fighter(Pokemon):
+    def attack(self, enemy):
+        super_power = randint(5,15)
+
+        self.power += super_power
+
+        result = super().attack(enemy)
+
+        self.power -= super_power
+        
+        return result + f"\
+Боец применил супер-атаку силой:{super_power} "
+    pass
+class Wizard(Pokemon):
+    pass
+
+wizard = Wizard("username1")
+fighter = Fighter("username2")
+
+print(wizard.info())
+print()
+print(fighter.info())
+print()
+print(fighter.attack(wizard))
+
